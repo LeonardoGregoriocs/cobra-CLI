545806088ae0b2f3c877640fdab8e3552db7b1b4
leonardogregoriocs <leonardogregoriocs@gmail.com>
2024-01-16 16:36:39 -0300

    :fire: feature - edit short description

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,7 +12,7 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
+	Short: "list category",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
